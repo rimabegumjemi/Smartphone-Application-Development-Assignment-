19c97b5944089cd8b33ee7ca0adf43caaab5539f
Rima Begum Jemi - 014 <rimabegumjemi29@gmail.com>
2025-11-06 21:15:12 +0000

    Initial commit

---
+++ project_62_a/lib/HomePage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget
+ {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 31, 22, 94),
+        foregroundColor: Colors.white,
+        // leading: Icon(Icons.home, color: Colors.amber),
+        title: Text("HomePage"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.logout)),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+
+      ),
+
+      drawer: NavigationDrawer(children: [
+          DrawerHeader(child: UserAccountsDrawerHeader
+          (
+              accountName: Text("Name"),
+              accountEmail: Text("Email"),
+              
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.person),
+            title: Text("Profile"),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
